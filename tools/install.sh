73e1b45951c412977fee829bd3fffeb724bee6d6
Ian Meyer <ianmmeyer@gmail.com>
2009-10-09 12:41:46 -0400

    Added -p option to specify which protocol to clone from.

--- tools/install.sh
+++ tools/install.sh
@@ -1,10 +1,27 @@
+# Allow ability to specify a protocol for git in the event git:// is 
+# firewalled and/or any other reason you may have.
+while getopts ":p:" OPTION
+do
+  case $OPTION in
+    p) protocol=$OPTARG;;
+    \?) echo "Invalid argument: -${OPTARG}." && usage >&2;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+# Default to git:// if $protocol is not set.
+if [ -z $protocol ]
+then
+  protocol=git
+fi
+
 if [ -d ~/.oh-my-zsh ]
 then
   echo "You already have Oh My Zsh installed. You'll need to remove ~/.oh-my-zsh if you want to install"
   exit
 else
   echo "Cloning Oh My Zsh..."
-  /usr/bin/env git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  /usr/bin/env git clone ${protocol}://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 
 echo "Looking for an existing zsh config..."
